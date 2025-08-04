a798469a2675f9ba6353419d2713965c4b0ba48a
Sultan <bransyahtan@gmail.com>
2025-08-04 16:11:17 +0700

    create detail recipes

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -11,6 +11,7 @@
           <p class="text-xl lg:text-2xl mb-8 text-balance">
             Discover recipes helping you to find the easiest way to cook.
           </p>
+          <MyButton to="#recipes" label="Browse Recipes" />
         </div>
         <div class="flex-1 order-1 lg:order-2">
           <NuxtImg
@@ -23,7 +24,39 @@
         </div>
       </div>
     </section>
+    <section id="recipes" class="py-20 container">
+      <h2 class="text-3xl lg:text-5xl mb-2">Discover, Create, Share</h2>
+      <p class="text-lg lg:text-xl mb-8">Check out our most popular recipes!</p>
+      <div
+        v-if="!error"
+        class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-8"
+      >
+        <RecipeCard v-for="recipe in data?.recipes" :recipe="recipe" />
+      </div>
+      <p v-else class="text-xl">
+        Opps, something went wrong. Please try again later
+      </p>
+    </section>
   </main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { RecipeResponse } from "~/types/types";
+
+const { data, error } = await useFetch<RecipeResponse>(
+  "https://dummyjson.com/recipes?limit=12"
+);
+
+useSeoMeta({
+  title: "Nuxtcipes",
+  description: "Recipes for you to cook!",
+  ogTitle: "Nuxtcipes",
+  ogDescription: "Recipes for you to cook!",
+  ogImage: "/nuxt-course-hero.png",
+  ogUrl: `http:localhost:3000`,
+  twitterTitle: "Nuxtcipes",
+  twitterDescription: "Recipes for you to cook!",
+  twitterImage: "/nuxt-course-hero.png",
+  twitterCard: "summary",
+});
+</script>
